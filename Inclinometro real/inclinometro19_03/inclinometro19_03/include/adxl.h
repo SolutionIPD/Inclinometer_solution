c544e512e13c8f763c5b2731dbfe1d06ebd7ca8b
sabuti <lucasa.linhares1@gmail.com>
2026-04-09 12:18:22 -0300

    update do adxl, versao mais simples

--- Inclinometro real/inclinometro19_03/inclinometro19_03/include/adxl.h
+++ Inclinometro real/inclinometro19_03/inclinometro19_03/include/adxl.h
@@ -3,10 +3,8 @@
 
 #include "calib.h"
 #include "app.h"
-#include <PL_ADXL355.h>
-#define ADXL_ADDRESS     0x53
-
-extern PL::ADXL355 adxl355;
+#include "Adafruit_Sensor.h"
+#include "Adafruit_ADXL345_U.h"
 
 void adxl_init(void);
 void adxl_measurements(SENSOR_CONTROL_PTR sensorControl);
--- Inclinometro real/inclinometro19_03/inclinometro19_03/src/adxl.cpp
+++ Inclinometro real/inclinometro19_03/inclinometro19_03/src/adxl.cpp
@@ -1,41 +1,27 @@
 #include "adxl.h"
 
-
-
-PL::ADXL355 adxl355;
-
-
+Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(10);
 
 void adxl_init(void){
-auto range = PL::ADXL355_Range::range2g; //set range of operations
-    adxl355.beginI2C(ADXL_ADDRESS); //início da comunicação IIC
-    auto deviceInfo = adxl355.getDeviceInfo(); //Verificação da comunicação com o Adxl355
-    if (deviceInfo.deviceId != 0xED) {
-        Serial.println("Could not find ADXL355!");
-    while (1);
+  if(!accel.begin()) { // assume range 2g
+    Serial.println("ADXL345 não detectado!");
+    while(1);
   }
-    Serial.println("ADXL355 initialized successfully!");  
 
-  // Set ADXL355 range
-  adxl355.setRange(range);  
-  // Enable ADXL355 measurement
-  adxl355.enableMeasurement();
-  //adxl355.setOutputDataRate(PL::ADXL355_OutputDataRate::odr1000); caso queira ativar o LPF e a ODR do sensor: consultar datasheet e biblioteca pl
-  //adxl355.setHpfFrequency(PL::ADXL355_HpfFrequency::hpf24_7); caso queira mudar a frequencia do hpf do sensor
-
-  
+  Serial.println("ADXL iniciado com sucesso!");  
 }
 
 void adxl_measurements(SENSOR_CONTROL_PTR sensorControl){ //Medições
-  auto accelerations = adxl355.getAccelerations();  //Aquisição das acelerações
-  float h[3] = {accelerations.x, accelerations.y, accelerations.z}; //organização das acelerações em forma de matriz
+  sensors_event_t AccelEvent;
+  accel.getEvent(&AccelEvent);
+  
+  float h[3] = {AccelEvent.acceleration.x, AccelEvent.acceleration.y, AccelEvent.acceleration.z}; //organização das acelerações em forma de vetor
   float Ainv [3] [3] = {{0.983286,0.003250,0.000004},{0.003250,0.976451,0.002108},{0.000004,0.002108,0.958748}}; //coeficientes da matriz A inversa, retiradas da calibração com o magneto
   float b [3] = {-0.009334, 0.007129 , -0.041051}; //coeficientes de BIAS retirados da calibração com o magneto
 
-  /*int accelx=accelerations.x;
-  int accely=accelerations.y;
-  int accelz=accelerations.z;*/ // testes sem a calibração
-  
+  /*int accelx=AccelEvent.acceleration.x
+  int accely=AccelEvent.acceleration.y;
+  int accelz=AccelEvent.acceleration.z;*/ // testes sem a calibração
 
   calib_accel_init(&sensorCalib, h, Ainv, b); //utilização do algorítmo de calibração
   sensorControl->adxl_angl_x = atan2f(sensorCalib.acc_x_calib,(sqrt((sensorCalib.acc_y_calib*sensorCalib.acc_y_calib)+(sensorCalib.acc_z_calib*sensorCalib.acc_z_calib))))*(RAD_TO_DEG);
